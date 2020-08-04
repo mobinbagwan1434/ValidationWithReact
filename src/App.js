296bc545d3d91b3af7ed967fa18348056b2a8d6d
mobinbagwan1434 <58305797+mobinbagwan1434@users.noreply.github.com>
2020-08-04 10:31:04 +0530

    h1 added

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <h1 style={{color:"red"}}> Welcome To Cloud Server </h1>
+
+  );
+}
+
+export default App;
